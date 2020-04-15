2ce2ed702e30287d3727ae9a4b4a76c9d5d67139
Ashvin Kumaran <ashvinrobot360@gmail.com>
2020-04-15 12:01:29 -0700

    added script to install docker

---
+++ docker/install_docker.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+USER=`whoami`
+if [ "$USER" != "root" ]; then
+    echo ERROR: Run this script using sudo
+    exit 1
+fi
+
+apt-get update
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io -y
